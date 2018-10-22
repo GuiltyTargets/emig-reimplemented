049086e7cbfe25091ac781295aede16f79240172
Özlem Muslu <ozlemmuslu@gmail.com>
2018-10-22 11:37:15 +0200

    Bug fix + Formatting and documentation of R code

--- src/emig_reimplemented/GENHelper/R/TargetPrioritization.R
+++ src/emig_reimplemented/GENHelper/R/TargetPrioritization.R
@@ -3,9 +3,11 @@ library(caret) # classification
 library(pROC) #ROC curve
 library(AppliedPredictiveModeling)
 
+#' Perform a 10 times repeated 5-fold stratified cross validation and
+#' save AUC values to a file.
 target.prioritization.pipeline <- function() {
     ############################################################
-    ### Prepare data
+    ### Prepare data.
     ############################################################
     targets <- as.character(unlist(read.table(targets.path)))
 
@@ -19,10 +21,10 @@ target.prioritization.pipeline <- function() {
     # pre-format
     analysis <- na.omit(as.data.frame(analysis))
     rownames(analysis) <- analysis$GeneID
-    return(analysis[ , 2:6])
+    analysis <- analysis[, 2 : 6]
 
     ############################################################
-    ### Assess the performance of the method
+    ### Assess the performance of the method.
     ############################################################
     auc.mat <- matrix(0, nrow = 5, ncol = 10)
     for (i in 1 : 10) {
@@ -30,42 +32,61 @@ target.prioritization.pipeline <- function() {
         auc.mat[, i] <- aucs
     }
 
+    ############################################################
+    ### Save the evaluation results.
+    ############################################################
     write.table(colMeans(auc.mat),
-                file = auc.output.path,
-                fileEncoding = "UTF-8",
-                sep = "\t",
-                row.names = FALSE
+    file = auc.output.path,
+    fileEncoding = "UTF-8",
+    sep = "\t",
+    row.names = FALSE
     )
 }
 
+#' Perform cross validation on the input data set.
+#' 
+#' @param analysis A dataframe containing 4 network-based features and binary known/unknown target
+#' labels with Entrez ids as row names.
+#' @param k.fold Number of folds in the cross validation.
+#' @return AUC values of the evaluation.
 cross.validate <- function(analysis, k.fold=5) {
-  # splitting
-  folds <- createFolds(analysis$IsTarget, k = k.fold)
-  aucs = vector(mode="numeric", length = k.fold)
-  for (i in 1:k.fold) {
-    pre.train <- analysis[-folds[[i]], ]
-    pre.test <- analysis[folds[[i]], ]
-    result = classify.fold(pre.train, pre.test)
-    aucs[i] <- as.numeric(result$auc)
-  }
-  return(aucs)
+    # splitting
+    folds <- createFolds(analysis$IsTarget, k = k.fold)
+    aucs = vector(mode = "numeric", length = k.fold)
+    for (i in 1 : k.fold) {
+        pre.train <- analysis[- folds[[i]],]
+        pre.test <- analysis[folds[[i]],]
+        result = classify.fold(pre.train, pre.test)
+        aucs[i] <- as.numeric(result$auc)
+    }
+    return(aucs)
 }
 
+#' Classify proteins for being a target or not, and report resulting ranking of likelihoods and the
+#' AUC values.
+#' 
+#' @param training The training set.
+#' @param test.set The test set.
+#' @return Predicted likelihoods of being targets for the test set and AUC value of the evaluation. 
 classify.fold <- function(training, test.set){
-  # training
-  train_control <- trainControl(method="boot", number=100)
-  model <- train(x          = as.matrix(training[, 1:4]),
-                 y          = training$IsTarget,
-                 trControl  = train_control,
-                 method     = "glm",
-                 family     = "binomial")
-  # calculate the likelihoods of the test set and evaluate the model
-  test.set.likelihood <- predict(model, newdata=test.set[, 1:4], type = "prob")
-  roc.curve   <- roc(response = test.set$IsTarget,
-                     predictor = test.set.likelihood$yes,
-                     levels = rev(levels(test.set$IsTarget)))
+    # training
+    train_control <- trainControl(method = "boot", number = 100)
+    model <- train(
+        x = as.matrix(training[, 1 : 4]),
+        y = training$IsTarget,
+        trControl = train_control,
+        method = "glm",
+        family = "binomial"
+    )
+    # calculate the likelihoods of the test set and evaluate the model
+    test.set.likelihood <- predict(model, newdata = test.set[, 1 : 4], type = "prob")
+    roc.curve <- roc(
+        response = test.set$IsTarget,
+        predictor = test.set.likelihood$yes,
+        levels = rev(levels(test.set$IsTarget))
+    )
 
-  return(list(likelihoods = test.set.likelihood, auc = roc.curve$auc))
+    return(list(likelihoods = test.set.likelihood, auc = roc.curve$auc))
 }
 
 target.prioritization.pipeline()
